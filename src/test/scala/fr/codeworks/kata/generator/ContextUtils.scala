b8a16d1aa1c599d71a0530b06c9b888d187a3731
Denise NGUYEN <dn.ng.pro@gmail.com>
2020-08-26 10:12:36 +0200

    Fix typo in Kata conf

--- src/test/scala/fr/codeworks/kata/generator/ContextUtils.scala
+++ src/test/scala/fr/codeworks/kata/generator/ContextUtils.scala
@@ -2,4 +2,4 @@ package fr.codeworks.kata.generator
 
 import org.scalatest.{FunSpec, Matchers}
 
-trait ContextUtils extends FunSpec with Matchers {}
+trait ContextUtils extends FunSpec with Matchers
