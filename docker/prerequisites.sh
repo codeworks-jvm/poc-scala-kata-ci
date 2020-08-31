e36db84703b87e818fff3fdc7fc1fc4d3eb6c9ac
Denise NGUYEN <dn.ng.pro@gmail.com>
2020-08-31 16:54:52 +0200

    Finalize CI

---
+++ docker/prerequisites.sh
@@ -0,0 +1,5 @@
+docker volume create --name=kata_jenkins_data
+docker volume create --name=kata_sonarqube_conf
+docker volume create --name=kata_sonarqube_data
+docker volume create --name=kata_sonarqube_extensions
+docker volume create --name=kata_nexus_data
